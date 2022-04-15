98f8a3430fb57267c544bd5bcfbd10034e546723
qudalsrnt3x <qudalsrnt3x@gmail.com>
2022-04-15 11:32:29 +0900

    ch2. 옵저버 패턴 연습 (기상예보 디스플레이 추가)

---
+++ src/ch2/ForecastDisplay.java
@@ -0,0 +1,32 @@
+package ch2;
+
+public class ForecastDisplay implements Observer, DisplayElement{
+
+    private float currentPressure = 29.92f;
+    private float lastPressure;
+    private WeatherData weatherData;
+
+    public ForecastDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+
+    }
+
+    @Override
+    public void display() {
+        System.out.print("기상 예보: ");
+        if (lastPressure > currentPressure)
+            System.out.println("쌀쌀하며 비가 올 것 같습니다.");
+        else if (lastPressure == currentPressure)
+            System.out.println("지금과 비슷할 것 같습니다.");
+        else
+            System.out.println("날씨가 좋아지고 있습니다!");
+    }
+
+    @Override
+    public void update() {
+        lastPressure = currentPressure;
+        currentPressure = weatherData.getPressure();
+        display();
+    }
+}
--- src/ch2/WeatherStation.java
+++ src/ch2/WeatherStation.java
@@ -8,6 +8,8 @@ public class WeatherStation {
                 new CurrentConditionsDisplay(weatherData);
         HeatIndexDisplay heatIndexDisplay =
                 new HeatIndexDisplay(weatherData);
+        ForecastDisplay forecastDisplay =
+                new ForecastDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
