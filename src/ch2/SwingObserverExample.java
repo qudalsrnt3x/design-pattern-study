b3e4331ed924cd0df51847d28b9ef8bd26f0059c
qudalsrnt3x <qudalsrnt3x@gmail.com>
2022-04-15 10:58:43 +0900

    ch2. 옵저버 패턴 연습 (자바 스윙 버튼)

---
+++ src/ch2/SwingObserverExample.java
@@ -0,0 +1,45 @@
+package ch2;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SwingObserverExample {
+    JFrame frame;
+
+    public static void main(String[] args) {
+        SwingObserverExample example = new SwingObserverExample();
+        example.go();
+    }
+
+    public void go() {
+        frame = new JFrame();
+
+        JButton button = new JButton("할까? 말까?");
+        button.addActionListener(new AngelListener());
+        button.addActionListener(new DevilListener());
+
+        // 프레임 속성을 설정
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.CENTER, button);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+    class AngelListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("하지마! 아마 후회할 걸?");
+        }
+    }
+
+    class DevilListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("그냥 저질러 버려!!");
+        }
+    }
+
+
+}
